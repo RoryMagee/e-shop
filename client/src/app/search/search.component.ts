7b751ac029a26e0e48548aac6c8e85d72c0826f5
Rory Magee <rory.magee2@gmail.com>
2018-12-08 20:22:13 +0000

    fixes

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -29,7 +29,7 @@ export class SearchComponent implements OnInit {
   }
 
   get upper() {
-    return Math.min(this.content.hitsPerPage * (this.content.page * 1), this.content.nbHits);
+    return Math.min(this.content.hitsPerPage * (this.content.page + 1), this.content.nbHits);
   }
   
   async getProducts() {
